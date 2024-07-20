cafb3ba69a5367fcf615138b56014ab0b17514f9
t04dJ14n9 <jimyoung1113@gmail.com>
2024-07-20 20:59:12 +0800

    finish pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,5 +3,56 @@
 
 int main()
 {
-    pipe();
+    int p1[2];
+    pipe(p1); // parent to child
+    int p2[2];
+    pipe(p2);        // child to parent
+    if (fork() == 0) // child
+    {
+        char bufRead[100];
+        if (read(p1[0], bufRead, 100) > 0)
+        {
+            if (strcmp(bufRead, "114514") == 0)
+            {
+                fprintf(1, "%d: received ping\n", getpid());
+            }
+            else
+            {
+                fprintf(1, "%d: received wrong ping message\n", getpid());
+            }
+        }
+        else
+        {
+            exit(-1);
+        }
+        char *bufWrite = "114514";
+        if (write(p2[1], bufWrite, strlen(bufWrite)) == strlen(bufWrite))
+        {
+            exit(0);
+        }
+    }
+    else // parent
+    {
+        char *bufWrite = "114514";
+        if (write(p1[1], bufWrite, strlen(bufWrite)) == strlen(bufWrite))
+        {
+            char bufRead[100];
+            if (read(p2[0], bufRead, 100) > 0)
+            {
+                if (strcmp(bufRead, "114514") == 0)
+                {
+                    fprintf(1, "%d: received pong\n", getpid());
+                }
+                else
+                {
+                    fprintf(1, "%d: received wrong ping message\n", getpid());
+                }
+            }
+            else
+            {
+                exit(-1);
+            }
+        }
+    }
+    exit(0);
 }
