a2fa2e19158aea8319c2f5ec191617c2bc69c6af
t04dJ14n9 <jimyoung1113@gmail.com>
2024-07-27 18:17:15 +0800

    syscall trace completed

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -127,6 +127,31 @@ static uint64 (*syscalls[])(void) = {
     [SYS_trace] sys_trace,
 };
 
+static char *syscallnames[] = {
+    [SYS_fork] "fork",
+    [SYS_exit] "exit",
+    [SYS_wait] "wait",
+    [SYS_pipe] "pipe",
+    [SYS_read] "read",
+    [SYS_kill] "kill",
+    [SYS_exec] "exec",
+    [SYS_fstat] "fstat",
+    [SYS_chdir] "chdir",
+    [SYS_dup] "dup",
+    [SYS_getpid] "getpid",
+    [SYS_sbrk] "sbrk",
+    [SYS_sleep] "sleep",
+    [SYS_uptime] "uptime",
+    [SYS_open] "open",
+    [SYS_write] "write",
+    [SYS_mknod] "mknod",
+    [SYS_unlink] "unlink",
+    [SYS_link] "link",
+    [SYS_mkdir] "mkdir",
+    [SYS_close] "close",
+    [SYS_trace] "trace",
+};
+
 void syscall(void)
 {
   int num;
@@ -135,7 +160,11 @@ void syscall(void)
   num = p->trapframe->a7;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num])
   {
-    p->trapframe->a0 = syscalls[num]();
+    p->trapframe->a0 = syscalls[num](); // a0 is the return value
+    if ((p->tracemask >> num) % 2 == 1)
+    {
+      printf("syscall %s -> %d\n", syscallnames[num], p->trapframe->a0);
+    }
   }
   else
   {
@@ -149,6 +178,7 @@ uint64 sys_trace(void)
 {
   int mask;
   argint(0, &mask);
-  printf("mask = %d", mask);
+  struct proc *p = myproc();
+  p->tracemask = mask;
   return 0;
 }
